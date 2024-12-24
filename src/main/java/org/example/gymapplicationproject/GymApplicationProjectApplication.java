99e9f359d29e9c97c3ca520acbb8f4e4b53912b6
Viktor Kapustianyk <vityakap@gmail.com>
2024-12-24 13:02:00 +0200

    Initial commit

---
+++ src/main/java/org/example/gymapplicationproject/GymApplicationProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.gymapplicationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApplicationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymApplicationProjectApplication.class, args);
+    }
+
+}
